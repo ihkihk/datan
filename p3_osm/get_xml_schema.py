d25726d6abfe53ce9b16a447839d39ad0d990996
Ivailo Kassamakov <ivokassamakov@gmail.com>
2015-11-18 07:11:17 +0100

    Update

--- p3_osm/get_xml_schema.py
+++ p3_osm/get_xml_schema.py
@@ -6,8 +6,8 @@ from collections import Counter
 
 found_tags =  Counter()
 
-def count_tags(filename):
-    with open(filename, 'r') as inf:
+def count_tags(xmlf):
+    with xmlf as inf:
         parser = ET.iterparse(inf, events=('end',))
         for _, elem in parser:
                 found_tags[elem.tag] += 1
@@ -16,18 +16,29 @@ def count_tags(filename):
 
 tag_tree = {}
 
-def obtain_tree(xmlf):
-    with open(xmlf, 'r') as inf:
+def reconstruct_tree(xmlf):
+    with xmlf as inf:
         parser = ET.iterparse(inf)
+        _, root = parser.next()
+        tag_tree[root] = {}
+        cur_level = tag_tree[root]
         for _, elem in parser:
-            if not elem.tag in tag_tree:
-                tag_tree[elem.tag] = {}
+            if elem.parent.tag in cur_level:
+                tag_tree[elem.parent.tag] = {}
     pass
 
 ZIP = {'bz2':'bunzip2', 'tbz':'tar -bxvf', 'tgz':'tar -zxvf', 'zip':'unzip'}
 
 def unzip_file(filename):
-    pass
+    ext = os.path.splitext(filename)[1].lower()[1:]
+
+    if ext == 'bz2':
+        import bz2
+        return bz2.BZ2File(filename)
+    else:
+        print("ERROR: Currently only bzip2 is supported")
+        sys.exit(3)
+
 
 
 if __name__ == '__main__':
@@ -40,16 +51,16 @@ if __name__ == '__main__':
         print("ERROR: No input file specified", file=sys.stderr)
         sys.exit(1)
 
-    ext = os.path.splitext(filename)[1].lower()
+    ext = os.path.splitext(filename)[1].lower()[1:]
     if ext in ZIP:
-        inf = prepare_file(filename)
-    elif ext == '.xml' or ext == '.osm':
-        inf = filename
+        inf = unzip_file(filename)
+    elif ext == 'xml' or ext == 'osm':
+        inf = open(filename, 'r')
     else:
         print("ERROR: Input file must have one of the following extensions: osm, xml, {!r}" .format(list(ZIP.keys())), file=sys.stderr)
         sys.exit(2)
 
     count_tags(inf)
 
-    obtain_tree(inf)
+    reconstruct_tree(inf)
 
