0a3205c32e516ae739ac0e8b832d0df916f664d3
Ivailo Kassamakov <ikassamakov@etel.ch>
2015-11-20 08:36:53 +0100

    Update

--- p3_osm/get_xml_values.py
+++ p3_osm/get_xml_values.py
@@ -2,19 +2,25 @@
 
 import xml.etree.cElementTree as ET
 import pprint
+import re
 import open_file
 
-def get_attrib_values(xmlf, xpath, attr):
-    values = set()
+def get_attrib_values(xmlf, xpath):
+
+    values = {}
+
     with xmlf as inf:
         parser = ET.parse(inf)
-        #_, root = next(parser)
-        findings = parser.findall(r"*")
-        pprint.pprint(findings)
+        for p in xpath:
+            attr = re.search(r"\[@(.*)\]", p).group(1)
+            print(p)
+            print(attr)
+            values[p] = set()
+            findings = parser.findall(p)
 
-        for f in findings:
-            if attr in f.attrib.keys():
-                set.add(f.attrib[attr])
+            for f in findings:
+                if attr in f.attrib.keys():
+                    values[p].add(f.attrib[attr])
     
     return values
 
@@ -30,9 +36,28 @@ if __name__ == '__main__':
         sys.exit(1)
 
     inf = open_file.open_file(filename)
-    vals = get_attrib_values(inf, r".//relation/member[@role]", "role")
+    vals = get_attrib_values(inf, [r".//node[@lat]",
+                                   r".//node[@lon]",
+                                   r".//node[@user]",
+                                   r".//node/tag[@k]",
+                                   r".//node/tag[@v]",
+                                   r".//relation[@user]",
+                                   r".//relation/member[@role]",
+                                   r".//relation/member[@type]",
+                                   r".//relation/tag[@k]",
+                                   r".//relation/tag[@v]",
+                                   r".//way[@user]",
+                                   r".//way/tag[@k]",
+                                   r".//way/tag[@v]"
+                                   ])
 
-    pprint.pprint(vals)
+    for n in vals.keys():
+        name = re.sub(r"[^a-zA-Z]+", "-", n)
+        name = name.strip("-")
+        valsdict = {}
+        valsdict[n] = vals[n]
+        with open("valattr-{}.txt".format(name), 'w') as of:
+            pprint.pprint(valsdict, stream=of)
 
 
 
