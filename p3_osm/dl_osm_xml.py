21ef1e099081a1b6cc248c67295d81fe8d746c9a
Ivailo Kassamakov <ikassamakov@etel.ch>
2015-11-26 18:22:47 +0100

    Update

---
+++ p3_osm/dl_osm_xml.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+import requests
+
+# Overpass API server
+
+#OVERPASS_URL = r"http://overpass.osm.ch/api/interpreter"
+OVERPASS_URL = r"http://overpass-api.de/api/interpreter"
+
+# Bounding box coordinates for Zurich-city (S, W, N, E)
+BBOX = (47.31858, 8.44574, 47.43819, 8.62684)
+
+
+MAP_QUERY = r"[out:xml];(node{BBOX};<;);out meta;".format(BBOX=BBOX)
+#MAP_QUERY = "area['name:de'~'Z.rich'][admin_level=8][place='city'];rel(pivot);out meta;".format(BBOX=BBOX)
+
+print(MAP_QUERY)
+
+r = requests.post(OVERPASS_URL, data=MAP_QUERY, stream = True)
+#print(r.text)
+#print(r.request.body)
+#print(r.status_code)
+
+with open("zurich.osm.xml", 'wb') as fd:
+    for chunk in r.iter_content(8192):
+        fd.write(chunk)
+
+
+
+
