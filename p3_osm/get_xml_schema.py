aa0b3318917959f27ce38de0e6558f392b92e71f
Ivailo Kassamakov <ikassamakov@etel.ch>
2015-12-04 17:33:31 +0100

    Update

--- p3_osm/get_xml_schema.py
+++ p3_osm/get_xml_schema.py
@@ -1,35 +1,28 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
+"""Obtain the XML schema of an OSM file."""
+
+from collections import Counter
 import xml.etree.cElementTree as ET
 import pprint
 import open_file
 
-from collections import Counter
-
-tags_counters =  Counter()
-
-def count_tags(xmlf):
-    with xmlf as inf:
-        parser = ET.iterparse(inf, events=('end',))
-        for ev, elem in parser:
-            tags_counters[elem.tag] += 1
-            elem.clear()
-    
-    pprint.pprint(tags_counters)
 
 tag_tree = {}
 tag_tree_counters = Counter()
+tag_counters = Counter()
 
-def add_elem_to_tree(tag, attr):
-    tag_tree_counters[tag] += 1
-    if tag in tag_tree.keys():
-        tag_tree[tag].union(attr)
-    else:
-        tag_tree[tag] = set(attr)
 
-def reconstruct_tree(xmlf):
+def parse_tree(xmlf):
+    """Find all tags and their attributes in the input XML file, and count them.
+
+    Tag hierarchy is represented in a "dot" way, e.g. "osm.role.member".
+
+    Args:
+        xmlf: FileObject input XML file, already open for reading
+    """
     with xmlf as inf:
-        parser = ET.iterparse(inf, events = ('start','end'))
+        parser = ET.iterparse(inf, events=('start', 'end'))
         tag = []
         cur_level = -1
         for ev, elem in parser:
@@ -39,32 +32,50 @@ def reconstruct_tree(xmlf):
                     tag.append(elem.tag)
                 else:
                     tag[cur_level] = elem.tag
-                attribs = list(elem.attrib.keys())
-                add_elem_to_tree('.'.join(tag), attribs)
+                attrs = list(elem.attrib.keys())
+                tag_path = '.'.join(tag)
+                tag_tree_counters[tag_path] += 1
+                if tag_path in tag_tree.keys():
+                    tag_tree[tag_path].union(attrs)
+                else:
+                    tag_tree[tag_path] = set(attrs)
             else:
-                if elem.text is not None or elem.tail is not None:
-                    add_elem_to_tree('.'.join(tag)+'.TEXT', [])
-
                 cur_level -= 1
                 tag = tag[:cur_level+1]
+                tag_counters[elem.tag] += 1
 
-    pprint.pprint(tag_tree)
-    pprint.pprint(tag_tree_counters)
-    pass
+                # Release the memory of the current tag and all its children
+                # This is acceptable since we will never access it again
+                # This saves enormous amounts of operational RAM
+                elem.clear()
 
 
 if __name__ == '__main__':
-    import sys
-     
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-    else:
-        print("ERROR: No input file specified", file=sys.stderr)
-        sys.exit(1)
+    from argparse import ArgumentParser
 
-    inf = open_file.open_file(filename)
-    count_tags(inf)
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("-a", "--attr", action="store_true",
+                        help="print the tree of the found tags and their attributes")
+    parser.add_argument("-f", "--flat", action="store_true",
+                        help="print flat statistics of the found tags")
+    parser.add_argument("-t", "--tree", action="store_true",
+                        help="print the tree of the found tags and their statistics")
+    parser.add_argument("filename", metavar="FILE",
+                        help="input OSM XML file (can be compressed or uncompressed)")
+    args = parser.parse_args()
 
-    inf = open_file.open_file(filename)
-    reconstruct_tree(inf)
+    if not (args.flat or args.attr or args.tree):
+        args.flat = True
+    if args.flat: print("> Flat mode enabled\n")
+    if args.attr: print("> Attribute mode enabled\n")
+    if args.tree: print("> Tree mode enabled\n")
 
+    inf = open_file.open_file(args.filename)
+    parse_tree(inf)
+
+    if args.flat:
+        pprint.pprint(tag_counters)
+    if args.attr:
+        pprint.pprint(tag_tree)
+    if args.tree:
+        pprint.pprint(tag_tree_counters)
